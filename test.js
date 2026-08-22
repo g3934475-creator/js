30e3c03e1e39c4b3854fa684167f1907cc34109e
g3934475-creator <g3934475@gmail.com>
2026-08-23 02:42:05 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,25 @@
 (function () {
-    var ins = document.createElement("ins");
-    ins.className = "682178b6";
-    ins.setAttribute("data-key", "68a618775d9cbab63821911cd7291dc1");
+    // 1. reklam
+    var ins1 = document.createElement("ins");
+    ins1.className = "682178b6";
+    ins1.setAttribute("data-key", "72bcea5007b68311485f471bef869f53");
 
-    var script = document.createElement("script");
-    script.async = true;
-    script.src = "https://cdn77.unnajd.com/5dd6f0ec.js";
+    var script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = "https://cdn77.unnajd.com/5dd6f0ec.js";
 
-    document.body.appendChild(ins);
-    document.body.appendChild(script);
+    document.body.appendChild(ins1);
+    document.body.appendChild(script1);
+
+    // 2. reklam
+    var ins2 = document.createElement("ins");
+    ins2.className = "682178b6";
+    ins2.setAttribute("data-key", "68a618775d9cbab63821911cd7291dc1");
+
+    var script2 = document.createElement("script");
+    script2.async = true;
+    script2.src = "https://cdn77.unnajd.com/5dd6f0ec.js";
+
+    document.body.appendChild(ins2);
+    document.body.appendChild(script2);
 })();
