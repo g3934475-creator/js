6b79c40048c568e4635a32b6e003d147335eaf8f
g3934475-creator <g3934475@gmail.com>
2026-08-23 01:12:28 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-document.write('<ins class="682178b6" data-key="481160222e2774a2dea76ca5130c0354"></ins>');
-var s = document.createElement('script');
-s.async = true;
-s.src = 'https://cdn77.unnajd.com/5dd6f0ec.js';
-document.head.appendChild(s);
+(function () {
+    var ins = document.createElement("ins");
+    ins.className = "682178b6";
+    ins.setAttribute("data-key", "481160222e2774a2dea76ca5130c0354");
+
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://cdn77.unnajd.com/5dd6f0ec.js";
+
+    document.body.appendChild(ins);
+    document.body.appendChild(script);
+})();
