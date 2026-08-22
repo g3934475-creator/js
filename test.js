77c375218da7830306e7c35dd247329e7e5c96f3
g3934475-creator <g3934475@gmail.com>
2026-08-22 20:35:33 +0300

    xcf

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
-<ins class="682178b6" data-key="481160222e2774a2dea76ca5130c0354"></ins>
-<script async src="https://cdn77.unnajd.com/5dd6f0ec.js"></script>
+(function () {
+    var ins = document.createElement("ins");
+    ins.className = "682178b6";
+    ins.setAttribute("data-key", "481160222e2774a2dea76ca5130c0354");
+
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://cdn77.unnajd.com/5dd6f0ec.js";
+
+    document.body.appendChild(ins);
+    document.body.appendChild(script);
+})();
