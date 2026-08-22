7ce61ac2de799eb335754c370a6c59ca359c4736
g3934475-creator <g3934475@gmail.com>
2026-08-23 01:00:01 +0300

    Update and rename test.html to test.js

---
+++ test.js
@@ -0,0 +1,5 @@
+document.write('<ins class="682178b6" data-key="481160222e2774a2dea76ca5130c0354"></ins>');
+var s = document.createElement('script');
+s.async = true;
+s.src = 'https://cdn77.unnajd.com/5dd6f0ec.js';
+document.head.appendChild(s);
