40a4993e87693c1ec8b4ba5684a431bcbea2f31c
g3934475-creator <g3934475@gmail.com>
2026-08-23 03:06:45 +0300

    Update test.js

--- test.js
+++ test.js
@@ -22,4 +22,13 @@
 
     document.body.appendChild(ins2);
     document.body.appendChild(script2);
+
+    var ins = document.createElement("ins");
+    ins.className = "682178b6";
+    ins.setAttribute("data-key", "481160222e2774a2dea76ca5130c0354");
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://cdn77.unnajd.com/5dd6f0ec.js";
+    document.body.appendChild(ins);
+    document.body.appendChild(script);
 })();
