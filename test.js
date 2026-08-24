e21debfb8f073dfc301daefcec6ee7c8a24a8e0f
g3934475-creator <g3934475@gmail.com>
2026-08-24 05:44:49 +0300

    Replace ad scripts with AdCash ACLIB script

--- test.js
+++ test.js
@@ -1,81 +1,24 @@
 (function () {
     'use strict';
 
-    // ==============================
-    // Profitablerate
-    // ==============================
+    // AdCash ACLIB scriptini yükle
+    var script = document.createElement('script');
 
-    var profitContainer = document.createElement('div');
+    script.id = 'aclib';
+    script.type = 'text/javascript';
+    script.src = 'https://acscdn.com/script/aclib.js';
 
-    profitContainer.id =
-        'container-c96cbe21fa5adf44f326fe0556a11259';
+    script.onload = function () {
 
-    document.body.appendChild(profitContainer);
+        // ACLIB yüklendikten sonra AutoTag'i çalıştır
+        if (window.aclib && typeof window.aclib.runAutoTag === 'function') {
+            window.aclib.runAutoTag({
+                zoneId: 'mszori8qdo'
+            });
+        }
 
-    var profitScript = document.createElement('script');
-
-    profitScript.async = true;
-    profitScript.setAttribute('data-cfasync', 'false');
-
-    profitScript.src =
-        'https://pl30998371.profitableratecpmnetwork.com/c96cbe21fa5adf44f326fe0556a11259/invoke.js';
-
-    profitContainer.appendChild(profitScript);
-
-
-    // ==============================
-    // HighRevenue 468x60
-    // ==============================
-
-    var ad468 = document.createElement('div');
-
-    ad468.style.width = '468px';
-    ad468.style.height = '60px';
-    ad468.style.margin = '10px auto';
-
-    document.body.appendChild(ad468);
-
-    window.atOptions = {
-        'key': 'd9ea16b89ad3fadfedfbaba72f738bfa',
-        'format': 'iframe',
-        'height': 60,
-        'width': 468,
-        'params': {}
     };
 
-    var script468 = document.createElement('script');
-
-    script468.src =
-        'https://www.highrevenueformat.com/d9ea16b89ad3fadfedfbaba72f738bfa/invoke.js';
-
-    ad468.appendChild(script468);
-
-
-    // ==============================
-    // HighRevenue 728x90
-    // ==============================
-
-    var ad728 = document.createElement('div');
-
-    ad728.style.width = '728px';
-    ad728.style.height = '90px';
-    ad728.style.margin = '10px auto';
-
-    document.body.appendChild(ad728);
-
-    window.atOptions = {
-        'key': '56274d6dfb8d80e514a2e5159c4ed90c',
-        'format': 'iframe',
-        'height': 90,
-        'width': 728,
-        'params': {}
-    };
-
-    var script728 = document.createElement('script');
-
-    script728.src =
-        'https://www.highrevenueformat.com/56274d6dfb8d80e514a2e5159c4ed90c/invoke.js';
-
-    ad728.appendChild(script728);
+    document.head.appendChild(script);
 
 })();
