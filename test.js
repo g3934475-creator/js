c8539282942794bd255ae58026688423d4aea1bb
g3934475-creator <g3934475@gmail.com>
2026-08-24 18:41:22 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
 (function () {
-    var script = document.createElement("script");
+    var frame = document.createElement("div");
 
-    script.async = true;
-    script.src = "https://curoax.com/na/waWQiOjEyMzQyMDEsInNpZCI6MTc5NjA0OSwid2lkIjo3NDY2MzksInNyYyI6Mn0=eyJ.js";
+    frame.id = "frame";
+    frame.style.cssText =
+        "width:100%;margin:auto;position:relative;z-index:99998;";
 
-    document.head.appendChild(script);
+    var iframe = document.createElement("iframe");
+
+    iframe.setAttribute("data-aa", "2453181");
+    iframe.src = "//acceptable.a-ads.com/2453181/?size=Adaptive";
+    iframe.style.cssText =
+        "border:0;padding:0;width:70%;height:auto;overflow:hidden;display:block;margin:auto;";
+
+    frame.appendChild(iframe);
+
+    document.body.appendChild(frame);
 })();
