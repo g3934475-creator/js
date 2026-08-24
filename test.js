f3c3820dc5b93729da714711ef2fb4057183c26c
g3934475-creator <g3934475@gmail.com>
2026-08-24 16:25:07 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,8 @@
 (function () {
-    var wrapper = document.createElement("div");
-    wrapper.id = "aads-2453181";
-    wrapper.style.cssText =
-        "width:100%;margin:auto;position:relative;z-index:99998;";
+    var script = document.createElement("script");
 
-    var iframe = document.createElement("iframe");
-    iframe.setAttribute("data-aa", "2453181");
-    iframe.src = "//acceptable.a-ads.com/2453181/?size=Adaptive";
-    iframe.style.cssText =
-        "border:0;padding:0;width:70%;height:auto;overflow:hidden;display:block;margin:auto;";
+    script.async = true;
+    script.src = "https://curoax.com/na/waWQiOjEyMzQyMDEsInNpZCI6MTc5NjA0OSwid2lkIjo3NDY2MzksInNyYyI6Mn0=eyJ.js";
 
-    wrapper.appendChild(iframe);
-
-    // Sayfaya ekle
-    document.body.appendChild(wrapper);
+    document.head.appendChild(script);
 })();
