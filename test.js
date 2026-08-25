68fc312ddcc7c7ca13d4badc39d7a2185908a3b9
g3934475-creator <g3934475@gmail.com>
2026-08-25 04:41:13 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,52 +1,24 @@
 (function () {
-    function createAd(width, height, className, affQuery) {
-        var ins = document.createElement('ins');
+    // ACLIB kütüphanesini yükle
+    var script = document.createElement("script");
+    script.id = "aclib";
+    script.type = "text/javascript";
+    script.src = "https://acscdn.com/script/aclib.js";
 
-        ins.style.width = width + 'px';
-        ins.style.height = height + 'px';
+    script.onload = function () {
 
-        ins.setAttribute('data-width', width);
-        ins.setAttribute('data-height', height);
-        ins.className = className;
-        ins.setAttribute('data-domain', '//data527.click');
-        ins.setAttribute('data-affquery', affQuery);
+        // Banner reklam
+        if (typeof aclib !== "undefined") {
+            aclib.runBanner({
+                zoneId: "12039686"
+            });
 
-        var script = document.createElement('script');
-        script.src = '//data527.click/js/responsive.js';
-        script.async = true;
+            // Video Slider reklam
+            aclib.runVideoSlider({
+                zoneId: "12039698"
+            });
+        }
+    };
 
-        ins.appendChild(script);
-
-        return ins;
-    }
-
-    function loadAds() {
-        var container = document.body || document.documentElement;
-
-        // 0x0 reklam
-        container.appendChild(
-            createAd(
-                0,
-                0,
-                'a54ea9623cc',
-                '/d407969cc8dd27c7d117/54ea9623cc/?placementName=default'
-            )
-        );
-
-        // 300x250 reklam
-        container.appendChild(
-            createAd(
-                300,
-                250,
-                'u82d4f1e0fc',
-                '/421cc36fafeeca0a28a8/82d4f1e0fc/?placementName=default'
-            )
-        );
-    }
-
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', loadAds);
-    } else {
-        loadAds();
-    }
+    document.head.appendChild(script);
 })();
